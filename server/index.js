2139e3e9ca555847d0ec1662b181241f4038bc2e
manuel-heav <manuelheav32@gmail.com>
2022-08-08 15:09:56 +0300

    working on the express app for demonstration

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+app.use(morgan("common"));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello",
+  });
+});
+
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error("Not found" + req.originalUrl);
+  next(error);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: err.stack,
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Listening to ${port}`);
+});
