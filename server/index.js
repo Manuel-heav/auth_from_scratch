da39ee1c4f2b566d517945ab4bddc5ff17fceac5
manuel-heav <manuelheav32@gmail.com>
2022-08-08 15:15:33 +0300

    working on the express app for demonstration

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
+// const cors = require("cors");
 
 const app = express();
 
 app.use(morgan("common"));
-app.use(cors());
+// app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({
